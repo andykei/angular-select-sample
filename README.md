35a1d2d424743ef3bbfa3e32374a6de2dfe587d9
andykei <doxukei.a@gmail.com>
2019-12-21 19:35:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-select-sample
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-select-sample)
---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+import { SelectListComponent } from './select-list/select-list.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ,DragDropModule],
+  declarations: [ AppComponent,SelectListComponent],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
