df9e275d6f3a45f839efcd99fa57c98cd2e8be22
andykei <doxukei.a@gmail.com>
2019-12-22 21:00:35 +0900

    5 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 import { SelectListComponent } from './select-list/select-list.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ListFilterComponent } from './list-filter/list-filter.component';
+
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ,DragDropModule],
-  declarations: [ AppComponent,SelectListComponent],
+  imports:      [ BrowserModule, FormsModule ,DragDropModule,MaterialModule,BrowserAnimationsModule],
+  declarations: [ AppComponent,SelectListComponent, ListFilterComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
